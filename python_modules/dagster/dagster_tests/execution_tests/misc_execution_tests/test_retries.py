d31a0d4f38262341f0c3f871a05e67a8dbc396d8
prha <philrha@gmail.com>
2025-04-30 17:17:04 -0700

    retry resource failure init

--- python_modules/dagster/dagster_tests/execution_tests/misc_execution_tests/test_retries.py
+++ python_modules/dagster/dagster_tests/execution_tests/misc_execution_tests/test_retries.py
@@ -7,8 +7,10 @@ from collections.abc import Sequence
 import pytest
 from dagster import (
     Backoff,
+    ConfigurableResource,
     DagsterEventType,
     Failure,
+    InitResourceContext,
     Jitter,
     Out,
     Output,
@@ -646,3 +648,36 @@ def test_failure_metadata():
     assert step_failure_data
     assert step_failure_data.user_failure_data
     assert step_failure_data.user_failure_data.metadata["meta"].value == "data"
+
+
+class FailOnceResource(ConfigurableResource):
+    parent_dir: str
+
+    def create_resource(self, context: InitResourceContext) -> None:
+        filepath = os.path.join(self.parent_dir, f"{context.run_id}_resource.txt")
+        if not os.path.exists(filepath):
+            open(filepath, "a", encoding="utf8").close()
+            raise ValueError("Resource error")
+
+
+@op(retry_policy=RetryPolicy(max_retries=3))
+def do_something(my_resource: FailOnceResource):
+    pass
+
+
+@job(resource_defs={"my_resource": FailOnceResource(parent_dir="")})
+def resource_fail_once_job():
+    do_something()
+
+
+def test_resource_retries():
+    with tempfile.TemporaryDirectory() as tempdir:
+        with instance_for_test() as instance:
+            with execute_job(
+                reconstructable(resource_fail_once_job),
+                instance=instance,
+                raise_on_error=False,
+                run_config={"resources": {"my_resource": {"config": {"parent_dir": tempdir}}}},
+            ) as result:
+                assert result.success
+                assert len(_get_retry_events(result.events_for_node("do_something"))) == 1
